22bf5c42a1d4b45ba82b8ea18adfc90bc62335e0
whosaidhuh <huh@whosaidhuh.me>
2021-02-03 22:23:22 -0500

    added audio assets. began first file in repository to contain code

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import phonetics as ph
+from fuzzywuzzy import fuzz
+def phrase2keys(phrase):
+    """
+    Converts text to an mp3 of mapped piano sequences
+    :param phrase:
+    :return:
+    :example:
+    :TODO:
+    """
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
