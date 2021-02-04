2b32265783be4fa19cdb10bc1e174ed2a3c811f0
whosaidhuh <huh@whosaidhuh.me>
2021-02-03 23:55:26 -0500

    started text2piano method. converts text string generating piano sequence list

--- scratch.py
+++ scratch.py
@@ -16,5 +16,39 @@ def trim_audio_files():
         ffmpeg_extract_subclip(myfile, 0, .5, targetname=output)
 
 
+def text2piano(text: str):
+    """
+    Converts text string to piano sounds.
+    # text to metaphones
+    # metaphone to piano key
+    # generate piano key sequence
+    # write piano key sequence as mp3
+    # find audio files for piano keys in fingerprint
+    # concatenate audio files into one file
+    :param text:
+    :return: mp3 file
+    :example:
+    :TODO:
+    """
+    import phonetics
+    from os import walk
+    _notes = ['A2', 'A3', 'A4', 'C1',
+              'C2', 'C3', 'C4', 'C5', 'C6', 'Ds1', 'Ds2',
+              'Ds3', 'Ds4', 'Fs2', 'Fs3', 'Fs4']
+    _metaphones = list('0BFHJKLMNPRSTWXY')
+    _meta2piano = dict(zip(_metaphones, _notes))
+    text = "mnemonic fingerprint"
+    metaphones = list(phonetics.metaphone(text))
+    fingerprint = [_meta2piano[phone] for phone in metaphones]
+
+    mypath = '/home/a/projects/mnemonic_fingerprinter/assets/audio/'
+    _, _, filenames = next(walk(mypath))
+    filenames = [f'{mypath}{f}' for f in filenames]
+    print(filenames)
+
+
+
+
+
 if __name__ == '__main__':
     trim_audio_files()
