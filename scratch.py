e55526d2cee61a0e6f88ec4c85fb9de81a964fbc
whosaidhuh <huh@whosaidhuh.me>
2021-02-04 01:21:50 -0500

    added basic logging. works for warnings. need to change dispalyable log level

--- scratch.py
+++ scratch.py
@@ -1,3 +1,12 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from os import walk
+import phonetics
+from pydub import AudioSegment
+import logging
+logging.warning('Watch out!')  # will print a message to the console
+logging.info('I told you so')  # will not print anything
+
+
 def trim_audio_files(mypath: str, label: str, t0: float, t1: float):
     """
     Trims mp3 file using seconds
@@ -6,10 +15,7 @@ def trim_audio_files(mypath: str, label: str, t0: float, t1: float):
     :example:
     :TODO: logging. at least one test. any([label in fn for fn in filenames])
     """
-    from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-    from os import walk
-    import logging
-    logging.info('Starting')
+    logging.warning('Starting')
     _, _, filenames = next(walk(mypath))
     if False:
         filenames = [f'{mypath}{f}' for f in filenames]
@@ -17,9 +23,9 @@ def trim_audio_files(mypath: str, label: str, t0: float, t1: float):
             output = f'{myfile[:-4]}{label}{myfile[-4:]}'
             ffmpeg_extract_subclip(myfile, t0, t1, targetname=output)
     else:
-        logging.info(f'The label({label}) already exists!')
+        logging.warning(f'The label({label}) already exists!')
 
-    logging.info(f'Congratulations, there might be {label}s!')
+    logging.warning(f'Congratulations, there might be {label}s!')
 
 
 def text2piano(text: str):
@@ -36,9 +42,6 @@ def text2piano(text: str):
     :example:
     :TODO: at least one test. add rests.
     """
-    import phonetics
-    import logging
-    from pydub import AudioSegment
     _notes = ['A2', 'A3', 'A4', 'C1',
               'C2', 'C3', 'C4', 'C5', 'C6', 'Ds1', 'Ds2',
               'Ds3', 'Ds4', 'Fs2', 'Fs3', 'Fs4']
@@ -51,9 +54,7 @@ def text2piano(text: str):
     _mp3s = [AudioSegment.from_mp3(file) for file in filenames]
     _mp3 = sum(_mp3s)
     _outputmp3 = f"{mypath}{text}{''.join(metaphones)}.mp3".replace(' ','')
-    logging.info(_outputmp3)
-    print(text)
-    print(_outputmp3)
+    logging.warning(_outputmp3)
     _mp3.export(_outputmp3, format="mp3")
 
 
